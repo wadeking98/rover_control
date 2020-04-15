4760ab500668694d1d8e4650673d19f7f9662300
wadeking98 <wkingnumber2@gmail.com>
2020-04-14 21:10:26 -0700

    added grayscale streaming

--- rover.py
+++ rover.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 from socket import *
-from threading import Thread
+from threading import *
 import sys
 import time
 import numpy as np
 from picamera import PiCamera
 from picamera.array import PiRGBArray
+import gzip
 
 CON_ADDR = ()
 PORT = 5555
+FRAME_SEG_BUFF = []
+BUFF_SIZE = 5
+SEG_SIZE = 20
+PROD_SEM = Semaphore(BUFF_SIZE)
+CONS_SEM = Semaphore(0)
+BUFF_lock = Lock()
+
+
 
 def sendAndWait(msg_bytes, addr, sock, time=5):
     sock.settimeout(time)
@@ -35,21 +44,48 @@ def connect():
     sendAndWaitForRep("ack".encode(),addr,rsock,"ack")
     #if we dont receive an ack, that is okay, we assume the client
     #got the message
-    print(addr)
+    #print(addr)
     return
 
 
 def command_recv():
     while True:
         msg_enc, _ = rsock.recvfrom(1024)
-        print(msg_enc)
+        print(gzip.decompress(msg_enc))
     pass
 
 def frame_send():
     while True:
-        time.sleep(0.1)
-        rsock.sendto(bytes(0b1111),CON_ADDR)
+        CONS_SEM.acquire()
+        BUFF_lock.acquire()
+        rsock.sendto(gzip.compress(FRAME_SEG_BUFF.pop()), CON_ADDR)
+        # rsock.sendto(FRAME_SEG_BUFF.pop(), CON_ADDR)
+        BUFF_lock.release()
+        PROD_SEM.release()
     pass
+
+def frame_produce():
+    #print("got here")
+    camera = PiCamera(resolution='100x70', framerate=15)
+    rawCapture = PiRGBArray(camera)
+    time.sleep(0.1) #allow camera to warm up
+    while True:
+        camera.capture(rawCapture, format="bgr", use_video_port=True)
+        image = rawCapture.array
+        npimg = np.asarray(image, dtype=np.int32)
+        npimg = np.mean(npimg, axis=2)
+        npimg = npimg.astype(np.int)
+        imgshape = npimg.shape
+        npimg = npimg.flatten()
+        for i in range(0,len(npimg),SEG_SIZE):
+            PROD_SEM.acquire()
+            seg = np.insert(npimg[i:i+SEG_SIZE],0,[i,len(imgshape)])
+            seg = np.insert(seg, 2, imgshape)
+            FRAME_SEG_BUFF.insert(0, seg.tobytes())
+            #print(seg[0])
+            CONS_SEM.release()
+        rawCapture.truncate(0)
+
 
 if __name__ == "__main__":
     
@@ -66,21 +102,28 @@ if __name__ == "__main__":
 
     connthread = Thread(target=connect, name="conn_thread")
     cthread = Thread(target=command_recv, name="command_thread")
-    fthread = Thread(target=frame_send, name="frame_thread")
+    fthread1 = Thread(target=frame_send, name="frame_thread1")
+    fthread2 = Thread(target=frame_send, name="frame_thread2")
+    fthread3 = Thread(target=frame_send, name="frame_thread2")
+    prodthread = Thread(target=frame_produce, name="producer")
 
     #set up the camera
-    camera = PiCamera()
-    rawCapture = PiRGBArray(camera)
+    # camera = PiCamera()
+    # rawCapture = PiRGBArray(camera)
 
-    time.sleep(0.1)
+    # time.sleep(0.1)
 
-    camera.capture(rawCapture, format="bgr")
-    image = rawCapture.array
-    npimg = np.asarray_chkfinite(image)
+    # camera.capture(rawCapture, format="bgr")
+    # image = rawCapture.array
+    # print(image)
+    # npimg = np.asarray(image, dtype=np.int8)
 
-    print(npimg)
+    # print(np.frombuffer(npimg.tobytes(), dtype=np.int8))
 
     connthread.start()
     connthread.join()
     cthread.start()
-    fthread.start()
+    fthread1.start()
+    fthread2.start()
+    fthread3.start()
+    prodthread.start()
